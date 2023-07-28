f9d274665c2c8fb6cb481c07b44d3d09566a0280
Zain <learnerstudent001@gmail.com>
2023-07-28 12:06:55 +0500

    Initial Commit

---
+++ UserForm/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField(default=datetime.today)
+
+    def __str__(self):
+        return self.name
